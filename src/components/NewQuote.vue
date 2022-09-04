2a90eea4d4c2d10a83a435e4f05aea6025accdec
ryandi <purbaryandi@gmail.com>
2022-09-04 15:27:49 +0700

    finish

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="row">
     <form>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <div
+        class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"
+      >
         <label>Quote</label>
-        <textarea id="inputQuote" class="form-control" rows="3" v-model="quote"></textarea>
+        <textarea
+          id="inputQuote"
+          data-testid=textInputQuote
+          class="form-control"
+          rows="3"
+          v-model="quote"
+        ></textarea>
       </div>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <div
+        class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"
+      >
         <label>Color</label>
-        <select id="colorSelect" class="form-control" rows="3" v-model="color">
-          <option v-for="option in options" :value="option">{{ option }}</option>
+        <select id="colorSelect" data-testid=colourSelect class="form-control" rows="3" v-model="color">
+          <option v-for="option in options" :value="option">
+            {{ option }}
+          </option>
         </select>
       </div>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-        <button id="buttonAddQuote" class="btn btn-primary" @click.prevent="createNew">Add Quote
+      <div
+        class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"
+      >
+        <button
+          id="buttonAddQuote"
+          class="btn btn-primary"
+          data-testid=btnSubmitQuote
+          @click.prevent="createNew"
+        >
+          Add Quote
         </button>
       </div>
     </form>
@@ -23,20 +43,18 @@
 export default {
   data: function () {
     return {
-      quote: '',
-      color: '',
-      options: ["White", "Yellow", "Cyan", "Magenta", "Blue"]
+      quote: "",
+      color: "",
+      options: ["White", "Yellow", "Cyan", "Magenta", "Blue"],
     };
   },
   methods: {
     createNew() {
-      this.$emit('quoteAdded', {quote: this.quote, color: this.color});
-      this.quote = '';
-    }
-  }
-}
+      this.$emit("quoteAdded", { quote: this.quote, color: this.color });
+      this.quote = "";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
